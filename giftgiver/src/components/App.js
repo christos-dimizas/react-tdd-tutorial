cf0bbd01a5f2e25bb61a851703e4fe339440ea94
Christos Dimizas <christos.dimizas@marinetraffic.com>
2018-06-06 12:33:23 +0300

    Add index.js. Add render test for gift list

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gifts: [],
+        };
+    }
+
+    addGift = () => {
+        const {gifts} = this.state;
+        const ids = this.state.gifts.map(gift => gift.id);
+        const maxId = ids.length > 0 ? Math.max(...ids) : 0;
+        gifts.push({id: maxId + 1});
+        this.setState({gifts});
+    };
 
     render () {
         return (
         <div>
             <h2>Gift Giver</h2>
+            <div className="gift-list">
+                {this.state.gifts.map(gift => <div key={gift.id}>{gift.id}</div>)}
+            </div>
+            <Button
+                className="btn-add"
+                onClick={this.addGift}
+            >Add Gift</Button>
         </div>
         )
     }
--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import App from './App';
 
 const app = shallow(<App />);
 
 //
 it('Renders Correctly', () => {
-  expect(app).toMatchSnapshot();
+    expect(app).toMatchSnapshot();
 });
 
+it('Initializes the "state" with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+});
+
+it('Adds a new gift to "state" when clicking the "add gift" button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.state().gifts).toEqual([{id: 1}]);
+});
+
+it('Adds a new gift to the rendered list when clicking the "Add Gift" button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.find('.gift-list').children().length).toEqual(2); // 2 because of test pollution (clicked on the above test too)
+});
