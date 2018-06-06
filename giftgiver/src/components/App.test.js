6308d7731abd0ccb5d8d886d35d09a563b6e4f8b
Christos Dimizas <christos.dimizas@marinetraffic.com>
2018-06-06 13:17:05 +0300

    Refactor Tests to a more Behavior Driven Development. Also eliminate the issue with test pollution by adding beforeEach (which enables the click event) and afterEach (which resets the component state) methods

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -2,23 +2,33 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
 
-//
-it('Renders Correctly', () => {
-    expect(app).toMatchSnapshot();
-});
+// Behavior Driven Development. Organize relevant tests.
+describe('App', () => {
+    const app = shallow(<App />);
 
-it('Initializes the "state" with an empty list of gifts', () => {
-    expect(app.state().gifts).toEqual([]);
-});
+    it('Renders Correctly', () => {
+        expect(app).toMatchSnapshot();
+    });
 
-it('Adds a new gift to "state" when clicking the "add gift" button', () => {
-    app.find('.btn-add').simulate('click');
-    expect(app.state().gifts).toEqual([{id: 1}]);
-});
+    it('Initializes the "state" with an empty list of gifts', () => {
+        expect(app.state().gifts).toEqual([]);
+    });
 
-it('Adds a new gift to the rendered list when clicking the "Add Gift" button', () => {
-    app.find('.btn-add').simulate('click');
-    expect(app.find('.gift-list').children().length).toEqual(2); // 2 because of test pollution (clicked on the above test too)
+    describe('When clicking the "add-gift" button', () => {
+        beforeEach(() => {
+            app.find('.btn-add').simulate('click');
+        });
+        it('Adds a new gift to "state"', () => {
+            expect(app.state().gifts).toEqual([{id: 1}]);
+        });
+
+        it('Adds a new gift to the rendered list', () => {
+            expect(app.find('.gift-list').children().length).toEqual(1); // 2 because of  (clicked on the above test too)
+        });
+        // This removes test pollution issues.
+        afterEach(() => {
+            app.setState({gifts: []});
+        });
+    });
 });
