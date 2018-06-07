41c1d5c9913edb0dbcffb559bc9281912715c26a
Christos Dimizas <christos.dimizas@marinetraffic.com>
2018-06-07 12:37:59 +0300

    -Add an input field for adding to our balance. Include also testing

--- lootcheck/src/components/Wallet.js
+++ lootcheck/src/components/Wallet.js
@@ -4,12 +4,19 @@ import {connect} from 'react-redux';
 export class Wallet extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            balance: null,
+        }
     }
+
+    updateBalance = event => this.setState({balance: parseInt(event.target.value, 10)});
 
     render() {
         return (
             <div>
                 <h2 className="balance">Wallet Balance: {this.props.balance}</h2>
+                <br />
+                <input className="input-wallet" onChange={this.updateBalance}/>
             </div>
         );
     }
