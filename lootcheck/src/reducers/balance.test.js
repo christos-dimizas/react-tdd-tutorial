7650112481cf0cb76214a154653b9f9a05f93d28
Christos Dimizas <christos.dimizas@marinetraffic.com>
2018-06-07 12:23:51 +0300

    -Add Deposit and withdraw store actions (and reducer cases), including there tests.

--- lootcheck/src/reducers/balance.test.js
+++ lootcheck/src/reducers/balance.test.js
@@ -4,6 +4,30 @@ import * as constants from '../actions/constants';
 describe('BalanceReducer', () => {
     it('sets a balance', () => {
         const balance = 10;
-        expect(balanceReducer(null, {type: constants.SET_BALANCE, balance})).toEqual(balance);
+        expect(balanceReducer(
+            null,
+            {type: constants.SET_BALANCE, balance})
+        ).toEqual(balance);
+    });
+
+    it('deposits into the balance', () => {
+        const deposit = 10;
+        const initialState = 5;
+        expect(
+            balanceReducer(
+                initialState,
+                {type: constants.DEPOSIT, deposit})
+        ).toEqual(initialState + deposit);
+    });
+
+    it('withdraws from deposit', () => {
+        const withdraw = 5;
+        const initialState = 10;
+        expect(
+            balanceReducer(
+                initialState,
+                {type: constants.WITHDRAW, withdraw}
+            )
+        ).toEqual(initialState - withdraw);
     });
 });
